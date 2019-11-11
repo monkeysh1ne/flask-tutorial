cf3161e92bc9c3a475c80ffe47d25ecd9602eb67
monkeysh1ne <mcdoob@gmail.com>
2019-11-11 16:52:07 +1300

    Add templates for register, login & base

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -5,9 +5,22 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flask.rb import get_db
+from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
 
 
 
@@ -29,7 +42,7 @@ def register():
             error = 'User {} is already registered.'.format(username)
 
 
-        if error is None":
+        if error is None:
             db.execute(
                 'INSERT INTO user (username, password) VALUES (?, ?)',
                 (username, generate_password_hash(password))
@@ -58,13 +71,28 @@ def login():
         if user is None:
             error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
-            error = 'Incorrect poassword.'
+            error = 'Incorrect password.'
         
         if error is None:
             session.clear()
             session['user_id'] = user['id']
             return redirect(url_for('index'))
 
-            flash(error)
+        flash(error)
 
         return render_template('auth/login.html')
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect_url(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+    return wrapped_view
